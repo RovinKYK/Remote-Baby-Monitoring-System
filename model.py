718e46887766f5bbd64a9adfae28ae2f14a54255
RovinKYK <kykrovin@gmail.com>
2023-08-27 23:08:47 +0530

    Model modified to working state

--- model.py
+++ model.py
@@ -1,17 +1,19 @@
 import tensorflow as tf
 import tflite_model_maker as mm
 from tflite_model_maker import audio_classifier
+# import tempfile
 import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+from pydub.playback import play
 import seaborn as sns
 
-import itertools
+# import itertools
 import glob
 import random
 
-from IPython.display import Audio, Image
+# from IPython.display import Audio, Image
 from scipy.io import wavfile
 
 print(f"TensorFlow Version: {tf.__version__}")
@@ -23,64 +25,70 @@ print(f"Model Maker Version: {mm.__version__}")
                                                 cache_subdir='dataset',
                                                 extract=True)'''
 
-#Util functions and data structures.
-
 data_dir = './dataset'
 
+#'bu': 'burping'
 cry_code_to_name = {
-  'bp': 'belly pain',
-  'bu': 'burping',
-  'di': 'discomfort',
-  'hu': 'hungry',
-  'ti': "tired",
+    'bp': 'belly pain',
+    'di': 'discomfort',
+    'hu': 'hungry',
+    'ti': "tired",
 }
 
-'''crys_images = {
-  'bp': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Henicorhina_leucosticta_%28Cucarachero_pechiblanco%29_-_Juvenil_%2814037225664%29.jpg/640px-Henicorhina_leucosticta_%28Cucarachero_pechiblanco%29_-_Juvenil_%2814037225664%29.jpg', # 	Alejandro Bayer Tamayo from Armenia, Colombia
-  'bu': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/House_Sparrow%2C_England_-_May_09.jpg/571px-House_Sparrow%2C_England_-_May_09.jpg', # 	Diliff
-  'di': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Red_Crossbills_%28Male%29.jpg/640px-Red_Crossbills_%28Male%29.jpg', #  Elaine R. Wilson, www.naturespicsonline.com
-  'hu': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Chestnut-crowned_antpitta_%2846933264335%29.jpg/640px-Chestnut-crowned_antpitta_%2846933264335%29.jpg', # 	Mike's crys from Riverside, CA, US
-  'ti': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Synallaxis_azarae_76608368.jpg/640px-Synallaxis_azarae_76608368.jpg', # https://www.inaturalist.org/photos/76608368
-}'''
+test_files = os.path.join(data_dir, 'test/*/*.wav')
 
-test_files = os.path.join('/content', data_dir, 'test/*/*.wav')
 
 def get_random_audio_file():
-  test_list = glob.glob(test_files)
-  random_audio_path = random.choice(test_list)
-  return random_audio_path
+    test_list = glob.glob(test_files)
+    random_audio_path = random.choice(test_list)
+    return random_audio_path
+
+# audio_path = 'a\c\c\e.wav'
 
 
 def show_cry_data(audio_path):
-  sample_rate, audio_data = wavfile.read(audio_path, 'rb')
+    sample_rate, audio_data = wavfile.read(audio_path, 'rb')
 
-  cry_code = audio_path.split('/')[-2]
-  print(f'cry name: {cry_code_to_name[cry_code]}')
-  print(f'cry code: {cry_code}')
-  display(Image(crys_images[cry_code]))
+    cry_code = audio_path.split("\\")[-2]
+    print(audio_path)
+    print(f'cry code: {cry_code}')
+    print(f'cry name: {cry_code_to_name[cry_code]}')
 
-  plttitle = f'{cry_code_to_name[cry_code]} ({cry_code})'
-  plt.title(plttitle)
-  plt.plot(audio_data)
-  display(Audio(audio_data, rate=sample_rate))
+    title = f'{cry_code_to_name.get(cry_code, "Unknown")} ({cry_code})'
+    plt.title(title)
+    plt.plot(audio_data)
+    plt.show()
 
-print('functions and data structures created')
+    '''temp_wav_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
+    temp_wav_file.close()
+    temp_wav_filename = temp_wav_file.name
+    wavfile.write(temp_wav_filename, sample_rate, audio_data)
+
+    os.system(f'ffplay -nodisp -autoexit "{temp_wav_filename}"')
+    os.remove(temp_wav_filename)'''
+
+
+# show_cry_data(get_random_audio_file())
 
 spec = audio_classifier.YamNetSpec(
     keep_yamnet_and_custom_heads=True,
     frame_step=3 * audio_classifier.YamNetSpec.EXPECTED_WAVEFORM_LENGTH,
     frame_length=6 * audio_classifier.YamNetSpec.EXPECTED_WAVEFORM_LENGTH)
 
+print("Model created")
+
+
 train_data = audio_classifier.DataLoader.from_folder(
     spec, os.path.join(data_dir, 'train'), cache=True)
-train_data, validation_data = train_data.split(0.8)
+train_data, validation_data = train_data.split(0.9)
 test_data = audio_classifier.DataLoader.from_folder(
     spec, os.path.join(data_dir, 'test'), cache=True)
 
-batch_size = 128
+print("Dataset Loaded")
+
+batch_size = 10
 epochs = 100
 
-print('Training the model')
 model = audio_classifier.create(
     train_data,
     spec,
@@ -88,23 +96,32 @@ model = audio_classifier.create(
     batch_size=batch_size,
     epochs=epochs)
 
+print('Model Trained')
+
+
 print('Evaluating the model')
 model.evaluate(test_data)
 
+
 def show_confusion_matrix(confusion, test_labels):
-  """Compute confusion matrix and normalize."""
-  confusion_normalized = confusion.astype("float") / confusion.sum(axis=1)
-  axis_labels = test_labels
-  ax = sns.heatmap(
-      confusion_normalized, xticklabels=axis_labels, yticklabels=axis_labels,
-      cmap='Blues', annot=True, fmt='.2f', square=True)
-  plt.title("Confusion matrix")
-  plt.ylabel("True label")
-  plt.xlabel("Predicted label")
+    """Compute confusion matrix and normalize."""
+    # confusion_normalized = confusion.astype("float") / confusion.sum(axis=1)
+    confusion_normalized = confusion.astype("float")
+    axis_labels = test_labels
+    ax = sns.heatmap(
+        confusion_normalized, xticklabels=axis_labels, yticklabels=axis_labels,
+        cmap='Blues', annot=True, fmt='.2f', square=True)
+    plt.title("Confusion matrix")
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+    plt.show()
+
 
 confusion_matrix = model.confusion_matrix(test_data)
 show_confusion_matrix(confusion_matrix.numpy(), test_data.index_to_label)
+print("Confusion matrix created")
 
+'''
 serving_model = model.create_serving_model()
 
 print(f'Model\'s input shape and type: {serving_model.inputs}')
@@ -130,16 +147,15 @@ print(random_audio)
 results = []
 print('Result of the window ith:  your model class -> score,  (spec class -> score)')
 for i, data in enumerate(splitted_audio_data):
-  yamnet_output, inference = serving_model(data)
-  results.append(inference[0].numpy())
-  result_index = tf.argmax(inference[0])
-  spec_result_index = tf.argmax(yamnet_output[0])
-  t = spec._yamnet_labels()[spec_result_index]
-  result_str = f'Result of the window {i}: ' \
-  f'\t{test_data.index_to_label[result_index]} -> {inference[0][result_index].numpy():.3f}, ' \
-  f'\t({spec._yamnet_labels()[spec_result_index]} -> {yamnet_output[0][spec_result_index]:.3f})'
-  print(result_str)
-
+    yamnet_output, inference = serving_model(data)
+    results.append(inference[0].numpy())
+    result_index = tf.argmax(inference[0])
+    spec_result_index = tf.argmax(yamnet_output[0])
+    t = spec._yamnet_labels()[spec_result_index]
+    result_str = f'Result of the window {i}: ' \
+                 f'\t{test_data.index_to_label[result_index]} -> {inference[0][result_index].numpy():.3f}, ' \
+                 f'\t({spec._yamnet_labels()[spec_result_index]} -> {yamnet_output[0][spec_result_index]:.3f})'
+    print(result_str)
 
 results_np = np.array(results)
 mean_results = results_np.mean(axis=0)
@@ -151,4 +167,4 @@ print(f'Exporing the TFLite model to {models_path}')
 
 model.export(models_path, tflite_filename='my_crys_model.tflite')
 
-model.export(models_path, export_format=[mm.ExportFormat.SAVED_MODEL, mm.ExportFormat.LABEL])
+model.export(models_path, export_format=[mm.ExportFormat.SAVED_MODEL, mm.ExportFormat.LABEL])'''
