59c31972d81812518b23b48afdfa8fc2cfcdd171
RovinKYK <kykrovin@gmail.com>
2023-10-14 18:14:47 +0530

    Top panel of UI completed

--- app/src/main/java/com/example/baby_monitor_app/MainActivity.java
+++ app/src/main/java/com/example/baby_monitor_app/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.baby_monitor_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] modes = {"Sleeping", "Awaken", "Crying", "Disconnected"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autoCompleteTextView = findViewById(R.id.autoComplete);
+        adapter = new ArrayAdapter<String>(this,R.layout.list_item, modes);
+        autoCompleteTextView.setAdapter(adapter);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = adapter.getItem(position).toString();
+                Toast.makeText(MainActivity.this, "item " + item, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
