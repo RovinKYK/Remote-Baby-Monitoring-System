7aa9b3c84b2bd6237acb7f0de024fe76de5b7795
RovinKYK <kykrovin@gmail.com>
2023-10-23 22:03:17 +0530

    Menu change implemented

--- app/src/main/java/com/example/baby_monitor_app/MainActivity.java
+++ app/src/main/java/com/example/baby_monitor_app/MainActivity.java
@@ -6,17 +6,31 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] modes = {"Sleeping", "Awaken", "Crying", "Disconnected"};
+    String[] toast_msgs = {"Monitoring till baby awakes", "Monitors till baby cries", "Baby is crying!", "Device disconnected"};
+    int[] img_Src = {R.drawable.babymonitoring, R.drawable.babymonitoring, R.drawable.backai, R.drawable.poweroff};
+
+    ImageView main_img;
     AutoCompleteTextView autoCompleteTextView;
     ArrayAdapter<String> adapter;
+
+    protected void change_mode(int mode) {
+        Toast.makeText(MainActivity.this, toast_msgs[mode], Toast.LENGTH_SHORT).show();
+        main_img.setImageResource(img_Src[mode]);
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_img = findViewById(R.id.mainImg);
 
         autoCompleteTextView = findViewById(R.id.autoComplete);
         adapter = new ArrayAdapter<String>(this,R.layout.list_item, modes);
@@ -24,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = adapter.getItem(position).toString();
-                Toast.makeText(MainActivity.this, "item " + item, Toast.LENGTH_SHORT).show();
+                change_mode(position);
             }
         });
     }
